2d18446b3ac28fe6d12a1080022b756cb57a3934
Julien Polo <julien.polo@captive.fr>
2023-07-28 12:03:04 +0200

    🔧 Configure ESLint

---
+++ .eslint_todo.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  // ⚠️ Technical debt
+  // Theses rules should be fixed as soon as possible
+  overrides: [
+    // { files: ['*.*'], rules: {} },
+    // { files: ['*.ts'], rules: {} },
+  ],
+};
