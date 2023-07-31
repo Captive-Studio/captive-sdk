315103c5d8463fa16afd7de5ee760303d299b95f
Clément Prod'homme <clement.prod-homme@captive.fr>
2023-07-31 12:24:49 +0200

    ⚰️ Supprime le dossier test/ de captive-api

--- captive-api/test/captive/api_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Captive::APITest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert Captive::API::VERSION
-  end
-end
--- captive-api/test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Captive::API::Engine => "/captive-api"
-end
