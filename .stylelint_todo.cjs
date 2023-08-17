7b95d8eedfb149e5138d71fdf8274b808cb52420
Julien Polo <julien.polo@captive.fr>
2023-08-17 14:23:13 +0200

    🚨 Correct linter obvious errors

--- .stylelint_todo.cjs
+++ .stylelint_todo.cjs
@@ -2,4 +2,15 @@ module.exports = {
   // ⚠️ Technical debt
   // Theses rules should be fixed as soon as possible
   rules: {},
+
+  overrides: [
+    // ActiveAdmin does not support theses patterns
+    {
+      files: ['captive-admin/**'],
+      rules: {
+        'selector-id-pattern': null,
+        'selector-class-pattern': null,
+      },
+    },
+  ],
 };
